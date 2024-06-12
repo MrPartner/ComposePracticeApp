be74006c3198e85a839b9bb084d90aa23014718b
DanielDevs <danielplimpias@gmail.com>
2024-06-12 18:34:29 -0400

    terminamos Twiiter card

--- app/src/main/java/com/example/composepracticeapp/MainActivity.kt
+++ app/src/main/java/com/example/composepracticeapp/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.example.composepracticeapp.loginbasico.LoginScreen
-import com.example.composepracticeapp.tuit.MyTuit
+import com.example.composepracticeapp.tuit.TuitDivider
 import com.example.composepracticeapp.tuit.TwitterCard
 import com.example.composepracticeapp.ui.theme.ComposePracticeAppTheme
 
@@ -21,11 +24,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposePracticeAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Surface(modifier = Modifier.padding(innerPadding)) {
-                        TwitterCard()
+                Column (modifier = Modifier.fillMaxSize().background(Color(0xff161d26))){
+                    TwitterCard()
+                    TuitDivider()
 
-                    }
                 }
             }
         }
--- app/src/main/java/com/example/composepracticeapp/tuit/TwitterCard.kt
+++ app/src/main/java/com/example/composepracticeapp/tuit/TwitterCard.kt
@@ -2,7 +2,7 @@ package com.example.composepracticeapp.tuit
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -12,9 +12,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -24,12 +29,17 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composepracticeapp.R
 
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun TwitterCard() {
+    var chat by rememberSaveable { mutableStateOf(false) }
+    var rt by rememberSaveable { mutableStateOf(false) }
+    var like by rememberSaveable { mutableStateOf(false) }
+
     Row(
         Modifier
             .fillMaxWidth()
@@ -77,6 +87,62 @@ fun TwitterCard() {
                 contentScale = ContentScale.Crop
             )
             Row(modifier = Modifier.padding(top = 16.dp)) {
+                SocialIcon(
+                    modifier = Modifier.weight(1f),
+                    unSelectedIcon = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_chat),
+                            contentDescription = "",
+                            tint = Color(0xff7e8b98)
+                        )
+                    },
+                    selectedIcon = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_chat_filled),
+                            contentDescription = "",
+                            tint = Color(0xff7e8b98)
+                        )
+                    },
+                    isSelected = chat
+                ) { chat = !chat }
+
+                SocialIcon(
+                    modifier = Modifier.weight(1f),
+                    unSelectedIcon = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_rt),
+                            contentDescription = "",
+                            tint = Color(0xff7e8b98)
+                        )
+                    },
+                    selectedIcon = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_rt),
+                            contentDescription = "",
+                            tint = Color(0xff00ff27)
+                        )
+                    },
+                    isSelected = rt
+                ) { rt = !rt }
+
+                SocialIcon(
+                    modifier = Modifier.weight(1f),
+                    unSelectedIcon = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_like),
+                            contentDescription = "",
+                            tint = Color(0xff7e8b98)
+                        )
+                    },
+                    selectedIcon = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_like_filled),
+                            contentDescription = "",
+                            tint = Color(0xffff0000)
+                        )
+                    },
+                    isSelected = like
+                ) { like = !like }
 
             }
         }
@@ -88,10 +154,27 @@ fun SocialIcon(
     modifier: Modifier,
     unSelectedIcon: @Composable () -> Unit,
     selectedIcon: @Composable () -> Unit,
-    isSelected:Boolean,
+    isSelected: Boolean,
     onItemSelected: () -> Unit
 ) {
+    val defaultValue = 1
 
+    Row(
+        modifier = modifier.clickable { onItemSelected() },
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        if (isSelected) {
+            selectedIcon()
+        } else {
+            unSelectedIcon()
+        }
+        Text(
+            text = if (isSelected) (defaultValue + 1).toString() else defaultValue.toString(),
+            color = Color(0xff7e8b98),
+            fontSize = 12.sp,
+            modifier = Modifier.padding(start = 4.dp)
+        )
+    }
 }
 
 @Composable
@@ -108,4 +191,9 @@ fun TextTitle(title: String, modifier: Modifier = Modifier) {
 @Composable
 fun DefaultText(title: String, modifier: Modifier = Modifier) {
     Text(text = title, color = Color.Gray, modifier = modifier)
+}
+
+@Composable
+fun TuitDivider(){
+    Divider(Modifier.padding(top = 4.dp).height(0.5.dp).fillMaxWidth(), color = Color(0xff7e8b98))
 }
